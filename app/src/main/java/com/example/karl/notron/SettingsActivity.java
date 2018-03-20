f4f9c04f9c40f974a5cfa35c91181ff15c7288fc
varikap <popadicn@gmail.com>
2018-03-20 12:51:05 +0100

    3. vezbe

--- app/src/main/java/com/example/karl/notron/SettingsActivity.java
+++ app/src/main/java/com/example/karl/notron/SettingsActivity.java
@@ -1,14 +1,15 @@
 package com.example.karl.notron;
 
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        addPreferencesFromResource(R.xml.pref_general);
     }
 
     @Override
