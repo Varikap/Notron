d24b29c4c4614cc82e33ac9e0c0dd14ce1864ce4
varikap <popadicn@gmail.com>
2018-03-27 12:22:22 +0200

    sa vezbi 4

--- app/src/main/java/com/example/karl/notron/PostsActivity.java
+++ app/src/main/java/com/example/karl/notron/PostsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class PostsActivity extends AppCompatActivity {
 
@@ -18,6 +19,10 @@ public class PostsActivity extends AppCompatActivity {
         btnCreatePost.setOnClickListener(createListener);
         btnReadPost.setOnClickListener(readListener);
         btnSettings.setOnClickListener(settingsListener);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.posts_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 
     View.OnClickListener settingsListener = new View.OnClickListener() {
